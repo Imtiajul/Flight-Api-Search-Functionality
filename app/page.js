15177c76687a64f0b5b6d8ee7fd65695596fb594
Imtiajul islam <iimtiajul@gmail.com>
2024-05-26 08:13:14 +0600

    remove fetch comment for netlify

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Search from "@/_components/search/Search";
 import { Suspense } from "react";
 
 export default async function Home() {
-    // const response = await fetch(`${process.env.BASE_URL}/api/flight-segments`);
+    const response = await fetch(`${process.env.BASE_URL}/api/flight-segments`);
 
-    // const searchDetails = await response.json();
-    // console.log('searchDetails', searchDetails)
+    const searchDetails = await response.json();
+    console.log('searchDetails', searchDetails)
 
   return (
     <section className="bg-[#F6F3E9] h-screen max-h-screen relative grid place-items-center bg-[url('/hero-bg.jpg')] bg-cover bg-no-repeat bg-center">
@@ -17,9 +17,9 @@ export default async function Home() {
             <p className="my-2 text-[#5f5e5e] text-center">
                 We will provide you the best rate around the world!
             </p>
-            {/* <Suspense>
+            <Suspense>
               <Search getSearchDetails={searchDetails} />
-            </Suspense> */}
+            </Suspense>
         </div>
     </div>
 </section>
